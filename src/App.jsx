1820664be60507f8a968849ac4ece944ee7fdf14
freshman <cyrus@freshman.dev>
2025-10-19 17:44:33 -0400

    color bar and other tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import './App.css'
 import AudioPlayer from './AudioPlayer'
 import LifeMap from './LifeMap'
+import ColorBar from './ColorBar'
 
 function App() {
   return (
     <>
-      <div className='header'>Laura Tsang</div>
+      <div className='header'>
+        <ColorBar />
+      </div>
       <div className='content'>
-        <p>I'm a regional planner for the state of Massachusetts!</p>
-        <p>In my free time I enjoy tennis, other outdoor activities, and various crafts.</p>
-        <div/>
+        <div className='text-box'>
+          <p>I'm a regional planner for the state of Massachusetts!</p>
+          <p>In my free time I enjoy tennis, other outdoor activities, and various crafts.</p>
+        </div>
+        {/* <div/> */}
         <AudioPlayer 
           src="/golden.m4a"
           title="Golden"
           artist="HUNTR/X"
           albumArt="/golden.jpeg"
         />
-        <div/>
+        {/* <div/> */}
         <LifeMap />
       </div>
       <div className='footer'>
